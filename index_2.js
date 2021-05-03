0e48ace4d1ce0af75704962a3ea091d6fa95fe3a
lucasvictorc <83615682+lucasvictorc@users.noreply.github.com>
2021-05-03 19:19:34 -0300

    Update index_2.js

--- index_2.js
+++ index_2.js
@@ -12,12 +12,12 @@ var options =
 	},
 	identity: 
 	{
-		username: "lucasvictor97",
-		password: "oauth:3n9yvbgphk2rrmfka7ese9q65yzi5f",  
+		username: "cpt_TRS",
+		password: "oauth:ec24amaubemcys6a0dgj2pqdliv7jw",
 	},
-	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg',] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules',] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
-
+                
 var client = new tmi.client(options);
 
 	client.connect();
