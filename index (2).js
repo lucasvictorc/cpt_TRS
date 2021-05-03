a5f3722020d50d52699c59f315685a70dd7fb5ab
lucasvictorc <83615682+lucasvictorc@users.noreply.github.com>
2021-05-03 19:28:16 -0300

    Update index (2).js

--- index (2).js
+++ index (2).js
@@ -12,8 +12,8 @@ var options =
 	},
 	identity: 
 	{
-		username: "lucasvictor97",
-		password: "oauth:3n9yvbgphk2rrmfka7ese9q65yzi5f",  
+		username: "cpt_trs",
+		password: "oauth:ec24amaubemcys6a0dgj2pqdliv7jw",  
 	},
 	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg',] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
